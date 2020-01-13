c2da1b1e394b8328d982dc0d23cbcc3aa6f6e1c7
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-13 12:31:37 -0300

    improved display of applied coupons

--- src/components/CouponsApplied.js
+++ src/components/CouponsApplied.js
@@ -5,12 +5,18 @@ export default class CouponsApplied extends Component {
   render() {
     const AppliedCoupons = this.props.couponsApplied.map(coupon => (
       <div key={ coupon.code } >
-        <p> { coupon.code } { coupon.value } </p>
+        <p> 
+          { coupon.code } {" -> "}
+          { coupon.type == "PERCENTUAL" ? " - " + (coupon.value * 100) + " %" : "" }
+          { coupon.type == "FIXED" ? " - " + util.formatCurrency(coupon.value) : "" }
+          { coupon.type == "FREE-SHIPPING" ? "FREE-SHIPPING" : "" }
+        </p>
       </div>
     ));
 
     return (
       <div>
+        <p>Coupons: </p>
         { AppliedCoupons }
       </div>
     )
