1558a7375ddbad6d59f78cba7303f69b86e2aba8
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-13 11:10:44 -0300

    passed logic from App.js to ShoppingCart.js

--- src/App.js
+++ src/App.js
@@ -1,93 +1,14 @@
 import React, { Component } from "react";
-import "./css/App.css";
-import Products from "./components/Products.js"
-import Coupons from "./components/Coupons.js"
-import Cart from "./components/Cart.js"
+import ShoppingCart from "./components/ShoppingCart";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { products: [], cartItems: [], coupons: [], couponsApplied: [] };
-  }
-
-  componentWillMount() {
-    fetch("http://localhost:8000/products").then(res => res.json())
-      .then(data => this.setState({
-        products: data
-      }));
-
-    fetch("http://localhost:8000/coupons").then(res => res.json())
-      .then(data => this.setState({
-        coupons: data
-      }));
-  }
-
-  removeFromCart = (e, product) => {
-    this.setState(state => {
-      let cartItems = state.cartItems;
-
-      cartItems.forEach(item => {
-        if (item.id === product.id) {
-          item.count -= 1;
-
-          if(item.count <= 0){
-            cartItems = cartItems.filter(item => item.id !== product.id)
-          }
-        }
-      });
-
-      return { cartItems: cartItems };
-    });
-  }
-
-  removeAllFromCart = (e, product) => {
-    this.setState(state => {
-      const cartItems = state.cartItems.filter(item => item.id !== product.id);
-      return { cartItems: cartItems };
-    })
-  }
-
-  addToCart = (e, product) => {
-    this.setState(state => {
-      const cartItems = state.cartItems;
-
-      let found = false;
-      cartItems.forEach(item => {
-        if (item.id === product.id) {
-          item.count += 1;
-          found = true;
-        }
-      });
-
-      if(found){
-        return { cartItems: cartItems };
-      }
-      cartItems.push({ ...product, count: 1 });
-
-      return { cartItems: cartItems };
-    });
-  }
-
   render(){
     return (
       <div>
         <h1>Shopping Cart</h1>
         <hr />
-        <div>
-          <div className="App add-product">
-            <Products products={ this.state.products } 
-              addToCart={ this.addToCart }
-              removeFromCart={ this.removeFromCart } />
-            </div>
 
-            <div className="App coupon">
-              <Coupons coupons={ this.state.coupons } />
-            </div>
-
-            <div className="App total">
-              <Cart cartItems={ this.state.cartItems } removeAllFromCart={ this.removeAllFromCart } />
-            </div>
-        </div>
+        <ShoppingCart />
       </div>
     );
   }
---
+++ src/components/ShoppingCart.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react'
+import "../css/App.css";
+import Products from "./Products.js"
+import Coupons from "./Coupons.js"
+import Cart from "./Cart.js"
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [], cartItems: [], coupons: [], couponsApplied: [], couponCode: "" };
+  }
+
+  componentWillMount() {
+    fetch("http://localhost:8000/products").then(res => res.json())
+      .then(data => this.setState({
+        products: data
+      }));
+
+    fetch("http://localhost:8000/coupons").then(res => res.json())
+      .then(data => this.setState({
+        coupons: data
+      }));
+  }
+
+  removeFromCart = (e, product) => {
+    this.setState(state => {
+      let cartItems = state.cartItems;
+
+      cartItems.forEach(item => {
+        if (item.id === product.id) {
+          item.count -= 1;
+
+          if(item.count <= 0){
+            cartItems = cartItems.filter(item => item.id !== product.id)
+          }
+        }
+      });
+
+      return { cartItems: cartItems };
+    });
+  }
+
+  removeAllFromCart = (e, product) => {
+    this.setState(state => {
+      const cartItems = state.cartItems.filter(item => item.id !== product.id);
+      return { cartItems: cartItems };
+    })
+  }
+
+  addToCart = (e, product) => {
+    this.setState(state => {
+      const cartItems = state.cartItems;
+
+      let found = false;
+      cartItems.forEach(item => {
+        if (item.id === product.id) {
+          item.count += 1;
+          found = true;
+        }
+      });
+
+      if(found){
+        return { cartItems: cartItems };
+      }
+      cartItems.push({ ...product, count: 1 });
+
+      return { cartItems: cartItems };
+    });
+  }
+
+  changeEvent = (e) => {
+    this.state.couponCode = e.target.value;
+  }
+
+  addCoupon = () => {
+    this.state.coupons.forEach(coupon => {
+      if (coupon.code === this.state.couponCode) {
+        this.state.couponsApplied.push({ ...coupon });
+      }
+    });
+
+    this.forceUpdate();
+  };
+
+  render(){
+    return (
+      <div>
+        <div className="App add-product">
+          <Products products={ this.state.products } 
+            addToCart={ this.addToCart }
+            removeFromCart={ this.removeFromCart } />
+          </div>
+
+          <div className="App coupon">
+            <Coupons changeEvent={ this.changeEvent }
+              addCoupon={ this.addCoupon } 
+              couponsApplied={ this.state.couponsApplied }/>
+          </div>
+
+          <div className="App total">
+            <Cart cartItems={ this.state.cartItems } 
+              couponsApplied={ this.state.couponsApplied }
+              removeAllFromCart={ this.removeAllFromCart } />
+            </div>
+      </div>
+    );
+  }
+}
