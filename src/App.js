440818c6c88c82efae3833a466c63409ce354fa5
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-15 18:03:05 -0300

    css changed and added to App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShoppingCartContainer from "./components/ShoppingCartContainer.js";
 class App extends Component {
   render(){
     return (
-      <div>
+      <div className="App total-size">
         <h1>Shopping Cart</h1>
         <hr />
 
